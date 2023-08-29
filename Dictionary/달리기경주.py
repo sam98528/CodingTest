ade85f0687c4dcc6863881e241fe9c1a60511aaa
Seungwon Lee <sam98528@naver.com>
2023-08-29 18:13:06 +0900

    달리기 경주[프로그래머스]

---
+++ Dictionary/달리기경주.py
@@ -0,0 +1,15 @@
+def solution(players, callings):
+    answer = []
+    playersMap = dict()
+    index = 0
+
+    for player in players:
+        playersMap[player] = int(index)
+        index += 1
+
+    for call in callings:
+        index = int(playersMap[call])
+        playersMap[call] -= 1
+        playersMap[players[index-1]] += 1
+        players[index-1], players[index] = players[index], players[index-1]
+    return players

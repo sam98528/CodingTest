eaf4dd703f208aadefabd2d9c862022117eca0c9
Seungwon Lee <sam98528@naver.com>
2023-09-01 22:03:27 +0900

    [unrated] Title: 카드 뭉치, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.py
@@ -0,0 +1,16 @@
+def solution(cards1, cards2, goal):
+    answer = ''
+    cards1Stack, cards2Stack = 0,0
+    
+    
+    for temp in goal:
+        if cards1Stack < len(cards1) and cards1[cards1Stack] == temp:
+            cards1Stack += 1
+        elif cards2Stack < len(cards2) and cards2[cards2Stack] == temp:
+            cards2Stack += 1
+        else: 
+            answer = "No"
+    
+    if answer != "No":
+        answer = "Yes"
+    return answer

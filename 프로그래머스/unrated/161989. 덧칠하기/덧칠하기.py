50eb464db6bd8e7451bb961c98a0b2c10d367951
Seungwon Lee <sam98528@naver.com>
2023-09-01 22:02:57 +0900

    [unrated] Title: 덧칠하기, Time: 2.38 ms, Memory: 11.7 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/161989. 덧칠하기/덧칠하기.py
@@ -0,0 +1,12 @@
+def solution(n, m, section):
+    answer = 0
+    start = section[0]
+    answer += 1
+    
+    for item in section:
+        if(start + m > item):
+            continue
+        
+        start = item
+        answer+=1
+    return answer
